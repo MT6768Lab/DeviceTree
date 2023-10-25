eace30c9cf560fb9367eba1f289f1de87c22ffdd
Orkun Ergün <orkunergun68@gmail.com>
2023-10-25 14:31:49 +0000

    lancelot: Pixel-Ify!

--- device.mk
+++ device.mk
@@ -55,6 +55,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_galahad/android.hardware.nfc.uicc.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_galahad/com.android.nfc_extras.xml
 
+# Sign keys
+-include vendor/lineage-priv/keys/keys.mk
+
 # SKU-specific properties
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sku_props/build_ss_vendor.prop:$(TARGET_COPY_OUT_VENDOR)/build_ss_vendor.prop \
